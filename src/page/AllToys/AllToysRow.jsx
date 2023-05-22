ae1e8b895fe4363a02bae08117be4fb1e9022cab
rrtechmart <reza.cpbd@gmail.com>
2023-05-22 23:17:25 +0600

    Fully rearrange the all toys page

--- src/page/AllToys/AllToysRow.jsx
+++ src/page/AllToys/AllToysRow.jsx
@@ -2,7 +2,8 @@ import { Link } from "react-router-dom";
 
 
 const AllToysRow = ({ toy, handleDelete}) => {
-    const { name, price, category, _id, image, quantity, subcategories } = toy;
+    // console.log(toy);
+    const { toyName, price, sellerName, _id, quantity, subCategory } = toy;
     return (
         <tr>
             <th>
@@ -10,16 +11,13 @@ const AllToysRow = ({ toy, handleDelete}) => {
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                 </button>
             </th>
-            <td>
-                {name}
-            </td>
-            <td>
-                {subcategories}
-            </td>
+            <td>{sellerName} </td>
+            <td>{toyName} </td>
+            <td>{subCategory}</td>
             <td>{price}</td>
             <td> {quantity} </td>
-            <th>
-                
+
+            <th>                
                 <Link to={`/singleToy/${_id}`} className="btn btn-ghost btn-xs"> Details </Link>
             </th>
         </tr>
